479bfa476fae72915f17793323a6bb0fa00d206c
HP <lestariselvi27@gmail.com>
2024-10-07 20:08:37 +0700

    first commit

---
+++ app/src/main/java/Selvi/polbeng/ac/id/androidkotlinapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package Selvi.polbeng.ac.id.androidkotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvCounter: TextView
+    private lateinit var btnAddNumber: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvCounter = findViewById(R.id.tvCounter)
+        btnAddNumber = findViewById(R.id.btnAddNumber)
+
+        tvCounter.text = "1"
+    }
+
+    fun addNumber(v: View){
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        tvCounter.text = nextVal.toString()
+    }
+}
+
+
